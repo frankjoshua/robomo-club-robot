6d03ca4ca9306f135cbe0a724983b1e9469bf98f
josh <josh@tesseractmobile.com>
2019-10-20 22:33:06 -0500

    #13 Added ROS sourcing to .bashrc

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-vagrant ssh-config > .vagrant/ssh-config
-py.test --hosts=default --ssh-config=.vagrant/ssh-config
+# vagrant ssh-config > .vagrant/ssh-config
+py.test -qq --nagios --tb line --hosts=default --ssh-config=.vagrant/ssh-config
--- tests/test_testing_server.py
+++ tests/test_testing_server.py
@@ -1,10 +1,15 @@
-def test_passwd_file(host):
-    passwd = host.file("/etc/passwd")
-    assert passwd.contains("root")
-    assert passwd.user == "root"
-    assert passwd.group == "root"
-    assert passwd.mode == 0o644
+def test_bashrc(host):
+    bashrc = host.file("/home/operator/.bashrc")
+    assert bashrc.contains("source /opt/ros/melodic/setup.bash")
 
 def test_operator_is_user(host):
     user = host.user('operator')
     assert user.uid
+
+# def test_rostopic(host):
+#     cmd = host.run('rostopic list')
+#     assert cmd.rc == 0
+
+# def test_rostopic_exists(host):
+#     exists = host.exists('rostopic')
+#     assert exists
