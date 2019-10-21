217c32c9834dc3a64e2a6a157226273e87d60cdf
josh <josh@tesseractmobile.com>
2019-10-20 23:03:06 -0500

    Fixed anisble order

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-# vagrant ssh-config > .vagrant/ssh-config
-py.test -qq --nagios --tb line --hosts=default --ssh-config=.vagrant/ssh-config
+py.test -qq --nagios --tb line --hosts=default --ssh-config=ssh-config
