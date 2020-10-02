351a4990c1ea80541f316399a19b22f8383ba025
josh <josh@tesseractmobile.com>
2020-10-01 21:58:23 -0500

    Improved requirments loading

--- install.sh
+++ install.sh
@@ -3,4 +3,7 @@
 #Install Ansible Roles
 ansible-galaxy install -r ./ansible/requirements.yml
 #Run playbook to install softare on the Robot
+ansible-galaxy role install -f -r ./ansible/requirements.yml
+ansible-galaxy collection install -f -r ./ansible/requirements.yml
+ansible-playbook -i ./ansible/production ./ansible/ssh.yml --extra-vars "ansible_user=$USER" -Kk
 ansible-playbook -i ./ansible/production ./ansible/all.yml $@
