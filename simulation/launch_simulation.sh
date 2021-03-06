38a63b0b7a93abf0cb82151caf2f8c6931226ab2
josh <josh@tesseractmobile.com>
2021-03-06 12:34:56 -0600

    Jupyter notebook now working

--- simulation/launch_simulation.sh
+++ simulation/launch_simulation.sh
@@ -4,4 +4,6 @@ docker stop $(docker ps -aq)
 roslaunch simulation.launch &
 pid=$!
 docker start $(docker ps -aq)
+## Stop ros master because the simulation is running ros master
+docker stop ros_master
 wait "$pid"
