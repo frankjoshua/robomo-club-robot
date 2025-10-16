13f308c125a7d9491b85b8bff2e9f98119518eb6
Joshua Frank <josh@joshfrank.com>
2025-10-15 22:21:35 -0500

    Refactor ros_bash.sh to improve X11 permissions and streamline docker run command

--- ros_bash.sh
+++ ros_bash.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-xhost +
+xhost +local:root
+
 docker run --rm -it \
+  --env="DISPLAY=$DISPLAY" \
+  --env="QT_X11_NO_MITSHM=1" \
+  --env="LIBGL_ALWAYS_SOFTWARE=1" \
+  --env="MESA_GL_VERSION_OVERRIDE=3.3" \
+  -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
   -v "$(pwd):/app" \
   -w /app \
-  --network="host" \
-  --pid="host" \
-  --ipc="host" \
-  -e DISPLAY=$DISPLAY \
-  -v /tmp/.X11-unix:/tmp/.X11-unix \
-  ghcr.io/sloretz/ros:jazzy-desktop-full bash
-
+  --network=host \
+  ghcr.io/sloretz/ros:jazzy-desktop-full \
+  bash
